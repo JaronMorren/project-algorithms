7b15edd730adc8097609f1f12086066ac27172a5
Jaron Morren <jaronmorren@gmail.com>
2023-06-05 23:30:10 -0300

    test:wrote tests for encrypt message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+
+    # Valid types
+    assert encrypt_message("jazzbo", 100) == "obzzaj"
+
+    assert encrypt_message("jazzbo", 3) == "zaj_obz"
+
+    assert encrypt_message("jazzbo", 4) == "ob_zzaj"
+
+    # Invalid types
+    with pytest.raises(TypeError):
+        encrypt_message(13, 4)
+
+    with pytest.raises(TypeError):
+        encrypt_message("fake", "f")
